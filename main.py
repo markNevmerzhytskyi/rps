fbe2f6f124c221b53849c9b137fe51307b899c9d
markNevmerzhytskyi <mark.nevmerzh@gmail.com>
2023-09-28 17:03:01 +0200

    RSP Hello

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(10)
+import random
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+rsp = [rock, paper, scissors]
+a1 = random.choice(rsp)
+a2 = random.choice(rsp)
+print(a1)
+
+
+
+
+
