68781d12e8f5ef2999807688421103e0aaeda359
markNevmerzhytskyi <mark.nevmerzh@gmail.com>
2023-10-05 17:10:12 +0200

    RSP Hello

--- main.py
+++ main.py
@@ -9,24 +9,44 @@ countD = 0
 countPW = 0
 countRW = 0
 countSW = 0
+count_pc = 0
+count_my = 0
 
-n = 1000000
+n = 100
 for i in range(n):
-    a1 = random.choice(rsp)
+    a1 = input("Your choice: ")
     a2 = random.choice(rsp)
 
     if (a1 == rock and a2 == rock) or (a1 == paper and a2 == paper) or (a1 == scissors and a2 == scissors):
-        #print("Draw!")
+        print("Draw!")
         countD += 1
     if (a1 == rock and a2 == paper) or (a1 == paper and a2 == rock):
-        #print("Paper wins!")
+        print("Paper wins!")
         countPW += 1
+        if a1 == paper:
+            count_my += 1
+        else:
+            count_pc += 1
     if (a1 == rock and a2 == scissors) or (a1 == scissors and a2 == rock):
-        #print("Rock wins!")
+        print("Rock wins!")
         countRW += 1
+        if a1 == rock:
+            count_my += 1
+        else:
+            count_pc += 1
     if (a1 == paper and a2 == scissors) or (a1 == scissors and a2 == paper):
-        #print("Scissors win!")
+        print("Scissors wins!")
         countSW += 1
-print(f"Draw percentage: {countD / n * 100}%, paper win percentage: {countPW / n * 100}%,"
-      f" rock win percentage: {countRW / n * 100}%, scissors win percentage: {countSW / n * 100}%, ")
+        if a1 == scissors:
+            count_my += 1
+        else:
+            count_pc += 1
+    if count_my == 5:
+        print(f"Player wins! Score: {count_my} - {count_pc}")
+        break
+    if count_pc == 5:
+        print(f"Computer wins! Score: {count_pc} - {count_my}")
+        break
 
+# print(f"Draw percentage: {countD / n * 100}%, paper win percentage: {countPW / n * 100}%,"
+# f" rock win percentage: {countRW / n * 100}%, scissors win percentage: {countSW / n * 100}%, ")
