fd6c486bcbac6b8a3f6a7d49c8d6da0701672194
markNevmerzhytskyi <mark.nevmerzh@gmail.com>
2023-11-02 16:04:22 +0100

    Round

---
+++ Homework2.py
@@ -0,0 +1,34 @@
+# For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20.
+# Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values.
+# To avoid code repetition, write a separate helper "def round10(num):" and call it 3 times. Write the helper entirely below and at the same indent level as round_sum().
+# round_sum(16, 17, 18) → 60
+# round_sum(12, 13, 14) → 30
+# round_sum(6, 4, 4) → 10
+
+
+def round_sum(x, y, j):
+    round(x + y + j)
+    print(round(x + y + j))
+
+
+round_sum(23.6, 45.7, 25.7)
+
+
+def round_10(x):  # x >, < or = 5 = 90
+
+    str(x)
+    return x
+
+
+def round_15(y):
+    str(y)
+    return y
+
+
+def round_20(j):
+
+    str(j)
+
+    return j
+
+
