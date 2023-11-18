faab96d0c3610397b8c17f1b050ad4941f28ad80
markNevmerzhytskyi <mark.nevmerzh@gmail.com>
2023-11-18 11:02:44 +0100

    class Entity

---
+++ entity.py
@@ -0,0 +1,12 @@
+class Entity:
+    def __init__(self, x, y, name):
+        self.name = name
+        self.x = x
+        self.y = y
+
+
+ob = Entity(245, 150, "rock" "paper" "scissors")
+print(ob.x)
+print(ob.y)
+print(ob.name)
+
