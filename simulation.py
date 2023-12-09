d4f352ecc4271c2c8fbbf4cf2b70c3f8211dc8eb
markNevmerzhytskyi <mark.nevmerzh@gmail.com>
2023-12-09 12:00:15 +0100

    circle and color

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import pygame
 from screen import Screen
 from entity import Entity
+import random
 
 
 class Simulation:
@@ -9,13 +10,22 @@ class Simulation:
         self.rocks = []
         self.papers = []
         self.scissors = []
-        self.create_entity(10, 23, "rocks", 27)
+        self.rocks_color = (67, 45, 45)
+        self.papers_color = (150, 86, 56)
+        self.scissors_color = (56, 99, 52)
 
     def infinite_loop(self):
+        self.create_entity(300, 450, "rock", 27)
+        self.create_entity(100, 200, "paper", 27)
+        self.create_entity(500, 300, "scissors", 27)
         while 1 + 1 == 2:
             self.scr.screen.fill((100, 100, 100))
             for i in self.rocks:
-                pygame.draw.circle(self.scr.screen, (150, 45, 56), (i.x, i.y), 5)
+                pygame.draw.circle(self.scr.screen, self.rocks_color, (i.x, i.y), 5)
+            for i in self.papers:
+                pygame.draw.circle(self.scr.screen, self.papers_color, (i.x, i.y), 5)
+            for i in self.scissors:
+                pygame.draw.circle(self.scr.screen, self.scissors_color, (i.x, i.y), 5)
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -24,9 +34,13 @@ class Simulation:
 
     def create_entity(self, x, y, name, n):
         for i in range(n):
-            entity = Entity(x, y, name)
-            self.rocks.append(entity)
-            print(len(self.rocks))
+            entity = Entity(x + random.randint(0, 200), y + random.randint(0, 150), name)
+            if name == "rock":
+                self.rocks.append(entity)
+            if name == "paper":
+                self.papers.append(entity)
+            if name == "scissors":
+                self.scissors.append(entity)
 
 
 sim = Simulation()
